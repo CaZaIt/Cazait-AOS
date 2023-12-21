fade89b8fd94e117e986a0fefba95fd03984ecc8
bmsk <vv99911@gmail.com>
2023-12-21 18:40:23 +0900

    [Feat] 라이브러리 버전업

--- build-logic/src/main/kotlin/cazait.android.application.gradle.kts
+++ build-logic/src/main/kotlin/cazait.android.application.gradle.kts
@@ -1,11 +1,25 @@
+import gradle.kotlin.dsl.accessors._9885c8525475a2a77e0b650bdf1e3c81.androidTestImplementation
+import gradle.kotlin.dsl.accessors._9885c8525475a2a77e0b650bdf1e3c81.implementation
 import org.cazait.cazaitandroid.configureHiltAndroid
 import org.cazait.cazaitandroid.configureKotestAndroid
 import org.cazait.cazaitandroid.configureKotlinAndroid
+import org.cazait.cazaitandroid.libs
 
 plugins {
     id("com.android.application")
+    id("cazait.android.compose")
 }
 
 configureKotlinAndroid()
 configureHiltAndroid()
 configureKotestAndroid()
+
+dependencies {
+    val libs = project.extensions.libs
+    implementation(libs.findLibrary("hilt.navigation.compose").get())
+    implementation(libs.findLibrary("androidx.compose.navigation").get())
+    androidTestImplementation(libs.findLibrary("androidx.compose.navigation.test").get())
+
+    implementation(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+    implementation(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+}
