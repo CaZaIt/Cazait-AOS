7ca3751c064c3d069deea6d59b7485ebeaef1d63
bmsk <vv99911@gmail.com>
2023-12-29 02:27:13 +0900

    [Test] 유저 정보를 삭제하는 로직에 대한 테스트

--- core/repo/signin/impl/src/androidTest/kotlin/org/cazait/cazaitandroid/core/repo/signin/StoredUserPreferencesDataSourceTest.kt
+++ core/repo/signin/impl/src/androidTest/kotlin/org/cazait/cazaitandroid/core/repo/signin/StoredUserPreferencesDataSourceTest.kt
@@ -16,6 +16,7 @@ import org.cazait.cazaitandroid.core.repo.signin.api.model.RefreshToken
 import org.cazait.cazaitandroid.core.repo.signin.api.model.StoredUser
 import org.cazait.cazaitandroid.core.repo.signin.api.model.UserId
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.UUID
@@ -29,6 +30,11 @@ class StoredUserPreferencesDataSourceTest {
             scope = testCoroutineScope
         ) { testContext.preferencesDataStoreFile("test_datastore") }
     private val testDataSource = StoredUserPreferencesDataSource(testDataStore)
+
+    private val testUserId = UserId(UUID.randomUUID())
+    private val testAccountName = AccountName("TestUser")
+    private val testAccessToken = AccessToken("access-token-12345")
+    private val testRefreshToken = RefreshToken("refresh-token-12345")
 
     @Test
     fun 유저_정보가_저장되지_않았다면_null을_반환한다() {
@@ -45,11 +51,7 @@ class StoredUserPreferencesDataSourceTest {
     @Test
     fun 저장된_유저_정보는_불러오는_유저_정보와_같다() {
         testCoroutineScope.runTest {
-            val testUserId = UserId(UUID.randomUUID())
-            val testAccountName = AccountName("TestUser")
-            val testAccessToken = AccessToken("access-token-12345")
-            val testRefreshToken = RefreshToken("refresh-token-12345")
-
+            
             testDataSource.updateStoredUser(
                 StoredUser(
                     userId = testUserId,
@@ -65,6 +67,33 @@ class StoredUserPreferencesDataSourceTest {
             Assert.assertEquals(expectedStoredUser?.accountName, testAccountName)
             Assert.assertEquals(expectedStoredUser?.accessToken, testAccessToken)
             Assert.assertEquals(expectedStoredUser?.refreshToken, testRefreshToken)
+        }
+    }
+
+    @Test
+    fun 유저_정보를_삭제하고_불러오기하면_null이다() {
+        testCoroutineScope.runTest {
+            var expectedStoredUser = testDataSource.storedUserData.first()
+            Assert.assertNull(expectedStoredUser)
+
+            testDataSource.deleteStoredUser()
+            expectedStoredUser = testDataSource.storedUserData.first()
+            Assert.assertNull(expectedStoredUser)
+
+            testDataSource.updateStoredUser(
+                StoredUser(
+                    userId = testUserId,
+                    accountName = testAccountName,
+                    accessToken = testAccessToken,
+                    refreshToken = testRefreshToken
+                )
+            )
+            expectedStoredUser = testDataSource.storedUserData.first()
+            Assert.assertEquals(expectedStoredUser?.userId, testUserId)
+
+            testDataSource.deleteStoredUser()
+            expectedStoredUser = testDataSource.storedUserData.first()
+            Assert.assertNull(expectedStoredUser)
         }
     }
 }
