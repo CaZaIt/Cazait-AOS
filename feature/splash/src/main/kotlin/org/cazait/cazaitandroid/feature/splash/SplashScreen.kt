99e4dafc963a99f0ff39a56fb6cb9a6367272c9a
bmsk <vv99911@gmail.com>
2024-01-18 22:14:50 +0900

    [Fix] 스플래쉬 화면 애니메이션 수정

--- feature/splash/src/main/kotlin/org/cazait/cazaitandroid/feature/splash/SplashScreen.kt
+++ feature/splash/src/main/kotlin/org/cazait/cazaitandroid/feature/splash/SplashScreen.kt
@@ -2,8 +2,9 @@ package org.cazait.cazaitandroid.feature.splash
 
 import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.slideIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -24,7 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import org.cazait.cazaitandroid.core.designsystem.theme.CazaitTheme
@@ -34,9 +35,18 @@ import org.cazait.cazaitandroid.feature.splash.components.AppDescriptionCard
 internal fun SplashScreen(
     onClickStart: () -> Unit,
 ) {
+    var showImage by remember { mutableStateOf(false) }
+    val imageEnterTransition = fadeIn(animationSpec = tween(durationMillis = 3000)) + expandIn()
     var showBottomBar by remember { mutableStateOf(false) }
+    val descriptionCardEnterTransition =
+        fadeIn(animationSpec = tween(durationMillis = 3000)) + expandIn(
+            expandFrom = Alignment.BottomCenter,
+            initialSize = { IntSize(it.width, 0) },
+        )
+
     LaunchedEffect(Unit) {
-        delay(500)
+        showImage = true
+        delay(1500)
         showBottomBar = true
     }
 
@@ -50,8 +60,8 @@ internal fun SplashScreen(
             ) {
                 AnimatedVisibility(
                     modifier = Modifier.align(Alignment.Center),
-                    visible = true,
-                    enter = fadeIn(),
+                    visible = showImage,
+                    enter = imageEnterTransition,
                 ) {
                     Image(
                         imageVector = ImageVector.vectorResource(
@@ -68,9 +78,12 @@ internal fun SplashScreen(
         bottomBar = {
             AnimatedVisibility(
                 visible = showBottomBar,
-                enter = slideIn { IntOffset(0, 0) },
+                enter = descriptionCardEnterTransition,
             ) {
-                AppDescriptionCard(modifier = Modifier.fillMaxWidth(), onClickStart = onClickStart)
+                AppDescriptionCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClickStart = onClickStart,
+                )
             }
         },
     )
